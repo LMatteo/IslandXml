859ebc8fd2122b6c098d3bdb7b13cedca9d8b84c
LucasUnice <lucas.matteo@unice.fr>
2017-05-04 09:24:49 +0200

    changing in xml

---
+++ src/parser/TagFly.java
@@ -0,0 +1,13 @@
+package parser;
+
+public class TagFly implements TagBuilder {
+    @Override
+    public String getActionXml() {
+        return null;
+    }
+
+    @Override
+    public String getAnswerXml() {
+        return null;
+    }
+}
